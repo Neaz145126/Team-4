d545ad4d90a049c1e60659928a0e3ac1b68d8fc6
valnesta <76183708+valnesta@users.noreply.github.com>
2022-06-08 08:13:38 +0100

    Update CodeColor.ino

--- CodeColor.ino
+++ CodeColor.ino
@@ -1,24 +1,10 @@
 #include <Arduino.h>
 
 #define RED 0
-#define GREEN 1
-#define BLUE 2
-
-//Pin configuration
-const char HUE_S0 = 6;
-const char HUE_S1 = 5;
-const char HUE_S2 = 4;
-const char HUE_S3 = 3;
-const char HUE_OUT = 2;
-
-int rgbValues[3] = {};
-
-void redColorSensor();
-
+#d
 void setup() 
 {
   
-  Serial.begin(9600);        // Using standard baudrate
-
-  //Getting the pinmodes set
+  
+  
   
